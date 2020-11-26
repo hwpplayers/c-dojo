83b0f7ecc7fa9fab51c07c0ae609946c14f672d9
Mert Gör <mertgor@masscollabs.com>
2020-11-26 03:20:22 +0300

    directory update and OS theory information

---
+++ src/system/helloworld.c
@@ -0,0 +1,53 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2020, Masscollabs Services, Masscollaboration Labs, amassivus, procyberian, hwpplayers
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#include <stdio.h> // We'll discuss why we have started from stdio.h
+
+/**
+We'll discuss C programming lanugage and UNIX operating system. Their first program is "Hello World" so this is a tradition and where we'll start with another implementation called "paraphrase".
+**/
+
+int main()
+{
+    printf("Hello C Programming Language World\n"); // Why we write some symbols like \n or ; 
+
+    return 0;
+}
+
+/**
+That simple seems like simple but the main reason to write an output on screen is to test disk cpu and ram and other computing hardware if it is responsing or not.
+**/
+
+/*For now I write and compile code with GNU Compiler and support binaries for only Ubuntu but there is a long list for any operating system*/
+
+/*We'll discuss on computer architectures and computer science*/
+
